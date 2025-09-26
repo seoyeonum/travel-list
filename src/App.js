ab344487ae21312e074c3189fe746dc13fe42990
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-09-26 23:59:08 +0900

    ✨ feat: add state handler to get input value
    - add state handler to get select and input value from e.target.value
    - add return statement to pause the event when the description is empty

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: true },
@@ -20,22 +22,43 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e); // SyntheticBaseEvent > target > input 에서 입력값 확인 가능
+    // console.log(e); // SyntheticBaseEvent > target > input 에서 입력값 확인 가능
+
+    // description 을 작성하지 않았을 시 handleSubmit 중단
+    if (!description) return;
+
+    const newItem = { description, quantity, padked: false, id: Date.now() };
+    console.log(newItem);
+
+    // to set initial condition
+    setDescription('');
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 🛫 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       {/* button onClick 을 활용해도 가능하지만, 그 경우 enter 에는 작동 안함 */}
       <button>Add</button>
     </form>
