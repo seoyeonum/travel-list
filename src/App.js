ef62356e272bef1e818a5f7a8b1c9c93797fa17f
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-09-30 22:17:22 +0900

    ✨ feat: add item list into the PackingList
    - lift items state up as global to access the items from the PackingList
    - define the handleItem function in the global domain
    - use props to hand over the handleItem function to the Form
    - use props to hand over the items state to the PackingList
    - correct typo of item variable in Forms (padked → packed)

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,7 +27,7 @@ function Logo() {
   return <h1>🏝 Far Away 💼</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState(1);
 
@@ -32,8 +38,10 @@ function Form() {
     // description 을 작성하지 않았을 시 handleSubmit 중단
     if (!description) return;
 
-    const newItem = { description, quantity, padked: false, id: Date.now() };
+    const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+
+    onAddItems(newItem);
 
     // to set initial condition
     setDescription('');
@@ -65,11 +73,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
