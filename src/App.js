a49c45a63fd63842c7494569482aa6c0e05f86ec
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-10-03 22:40:25 +0900

    ✨feat: add calculating statistic in the Stats
    - add numItems and numPacked (derived state) that depends on item array
    - add percentage of packed items using template literal
    - render data earlier when the number of items is 0
    - revise commit of Oct 2nd on Oct 3rd

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -117,10 +117,28 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  // 위와 같은 작업을 통해 조기 렌더링이 가능하다.
+  // 즉, 불필요한 작업을 줄이고 빠르게 렌더링함
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and ypu already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? 'You got everything! Ready to go 😆'
+          : `💼 You have ${numItems} items on your list, and ypu already packed
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
